311df485d06031ab3ba3876fca7ea416a80bff72
Daniele Mazzotta <daniele.mazzotta@tii.ae>
2022-12-09 09:33:11 +0400

    more accent lines on description

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,13 @@
 import Planetary from "./Planetary";
 import styles from "../styles/Hero.module.css";
-import {useTranslation} from "next-export-i18n";
+import { useTranslation } from "next-export-i18n";
 import Logo from "../public/stichiboi-logo.svg";
 import DiscoverLine from "./generic/DiscoverLine";
+import { CSSProperties } from "react";
 
 export default function Hero(): JSX.Element {
-    const {t} = useTranslation();
-
+    const { t } = useTranslation();
+    const NUM_LINES = 6;
     return (
         <div className={styles.container}>
             <main className={styles.main}>
@@ -16,6 +17,7 @@ export default function Hero(): JSX.Element {
                         <h1 className={styles.title}>{t('hero.title')}</h1>
                     </div>
                     <hr className={`${styles.accentLine}`}/>
+
                     <div className={styles.descriptionContainer}>
                         <div className={`${styles.description}`}>
                             {t('hero.description')}
@@ -24,7 +26,17 @@ export default function Hero(): JSX.Element {
                             <br/>
                             {t('hero.description3')}
                         </div>
-                        <hr className={styles.descriptionLine}/>
+                        <div className={styles.descriptionLines}>
+                            {Array.from({ length: NUM_LINES }).map((_, i) => (
+                                <hr key={i}
+                                    className={`${styles.descriptionLine}`}
+                                    style={{
+                                        "--scaleY": Math.exp((NUM_LINES - i) / 3) / Math.exp(NUM_LINES / 3),
+                                        "--delay": `${0.1 * (i - 1)}s`
+                                    } as unknown as CSSProperties
+                                    }/>))
+                            }
+                        </div>
                     </div>
                 </div>
                 <Planetary/>
