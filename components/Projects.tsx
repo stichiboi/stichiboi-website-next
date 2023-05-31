e6317268d4d4fec8869be8df6d38f47b819b01dd
Daniele Mazzotta <daniele.mazzotta@tii.ae>
2023-05-31 14:22:25 +0400

    use spaces instead of indents

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,33 +8,33 @@ import { faBroomBall } from "@fortawesome/free-solid-svg-icons";
 import { SnowButton } from "./generic/SnowButton";
 
 export default function Projects(): JSX.Element {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <main id={"projects"} className={styles.container}>
-            <div className={styles.snow}>
-                <Snow/>
-            </div>
-            <div className={styles.project}>
-                <div className={styles.content}>
-                    <h3 className={styles.title}>{t("projects.sudoku.title")}</h3>
-                    <p className={styles.description}>{t("projects.sudoku.description")}</p>
-                    <SnowButton label={t("projects.cta")}
-                                icon={<FontAwesomeIcon icon={faBroomBall}/>}
-                                onClick={console.log}/>
-                </div>
-                <button className={styles.image} onClick={console.log}>
-                    <Image src={"/sudoku-hero.png"}
-                           width={687} height={667}
-                           alt={t("projects.sudoku.alt")}/>
-                </button>
-            </div>
-            <div>
+  return (
+    <main id={"projects"} className={styles.container}>
+      <div className={styles.snow}>
+        <Snow/>
+      </div>
+      <div className={styles.project}>
+        <div className={styles.content}>
+          <h3 className={styles.title}>{t("projects.sudoku.title")}</h3>
+          <p className={styles.description}>{t("projects.sudoku.description")}</p>
+          <SnowButton label={t("projects.cta")}
+            icon={<FontAwesomeIcon icon={faBroomBall}/>}
+            onClick={console.log}/>
+        </div>
+        <button className={styles.image} onClick={console.log}>
+          <Image src={"/sudoku-hero.png"}
+            width={687} height={667}
+            alt={t("projects.sudoku.alt")}/>
+        </button>
+      </div>
+      <div>
                 Reflexo
-            </div>
-            <div>
+      </div>
+      <div>
                 Clock
-            </div>
-        </main>
-    );
+      </div>
+    </main>
+  );
 }
--- components/generic/SnowButton.tsx
+++ components/generic/SnowButton.tsx
@@ -4,9 +4,9 @@ import Button, { ButtonProps } from "./Button";
 import styles from "../../styles/Snow.module.css";
 
 export function SnowButton(props: ButtonProps): JSX.Element {
-    return (
-        <Button {...props}>
-            <Snow className={styles.snowButton} drawProbability={.02}/>
-        </Button>
-    )
+  return (
+    <Button {...props}>
+      <Snow className={styles.snowButton} drawProbability={.02}/>
+    </Button>
+  )
 }
