57a3c3d35562db1a3e7e4ab42e1b9cb8eceb0705
Daniele Mazzotta <daniele.mazzotta@tii.ae>
2023-06-03 10:21:04 +0400

    fixed stepper save logic

--- projects/common/stepper/Stepper.tsx
+++ projects/common/stepper/Stepper.tsx
@@ -23,38 +23,44 @@ export function Stepper({
   displayValue
 }: StepperProps) {
 
-  const [value, setValue] = useState((max + min) / 2);
-  const getValue = useCallback(() => {
-    return parseInt(localStorage.getItem(saveKey) || '3');
+  const [value, setValue] = useState<number>();
+  useEffect(() => {
+    const saved = localStorage.getItem(saveKey);
+    if (saved) {
+      const value = parseInt(saved);
+      if (!isNaN(value)) {
+        setValue(value);
+      }
+    }
   }, [saveKey]);
 
   useEffect(() => {
-    setValue(getValue());
-  }, [getValue, saveKey]);
-
-  useEffect(() => {
-    localStorage.setItem(saveKey, value.toString());
-    onChange(value);
+    if (value !== undefined) {
+      localStorage.setItem(saveKey, value.toString());
+      onChange(value);
+    }
   }, [onChange, saveKey, value]);
-
-  const changeTriesCount = useCallback((modifier: number) => {
-    setValue(prev => {
-      if (min !== undefined && prev + modifier < min || max !== undefined && prev + modifier > max) {
-        return prev;
+  const updateValue = useCallback((modifier: -1 | 1) => {
+    setValue(prev =>{
+      if(!prev){
+        // return middle between max and min
+        return (max + min) / 2;
       }
-      return prev + modifier;
-    });
+      const tempValue = prev + modifier;
+      // clip the value between max and min
+      return Math.min(max, Math.max(min, tempValue));
+    })
   }, [max, min]);
 
   return (
     <div className={styles.container}>
       {label ? <h3>{label}</h3> : null}
       <div className={styles.stepper}>
-        <button onClick={() => changeTriesCount(-1)}>
+        <button onClick={() => updateValue(-1)}>
           {leftIcon}
         </button>
-        <p>{displayValue ? displayValue(value) : value}</p>
-        <button onClick={() => changeTriesCount(1)}>
+        <p>{(displayValue && value) ? displayValue(value) : value}</p>
+        <button onClick={() => updateValue(1)}>
           {rightIcon}
         </button>
       </div>
