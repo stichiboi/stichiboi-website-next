f2f77a1ae5c63c7fbf9a12ae1fceeaf6ede0133b
Daniele Mazzotta <daniele.mazzotta@tii.ae>
2023-06-05 14:32:05 +0400

    added background in the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,26 +1,32 @@
 import styles from "../styles/Footer.module.css";
 import { useTranslation } from "next-export-i18n";
 import React from "react";
+import Image from "next/image";
 
 export function Footer(): JSX.Element {
   const { t } = useTranslation();
   return (
-    <footer id={"footer"} className={styles.container}>
-      <div className={styles.main}>
-        <p>{t("footer.text_1")}</p>
-        <p>{t("footer.text_2")}</p>
+    <footer id={"footer"} className={styles.footer}>
+      <div className={styles.background}>
+        <Image src={"/footer-background-pattern.png"} alt={"null"} width={1280} height={1280}/>
       </div>
-      <p className={styles.copyright}>{t("footer.copyright")}</p>
-      <button
-        title={t("footer.backToTop")}
-        className={styles.backToTop}
-        onClick={() => {
-          document.getElementById("hero")?.scrollIntoView({
-            behavior: "smooth", block: "start"
-          })
-        }}>
-        <p>{"↑"}</p>
-      </button>
+      <div className={styles.container}>
+        <div className={styles.main}>
+          <p>{t("footer.text_1")}</p>
+          <p>{t("footer.text_2")}</p>
+        </div>
+        <p className={styles.copyright}>{t("footer.copyright")}</p>
+        <button
+          title={t("footer.backToTop")}
+          className={styles.backToTop}
+          onClick={() => {
+            document.getElementById("hero")?.scrollIntoView({
+              behavior: "smooth", block: "start"
+            })
+          }}>
+          <p>{"↑"}</p>
+        </button>
+      </div>
     </footer>
   );
 }
