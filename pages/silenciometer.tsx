09e2318ceaa707aca09a01fab76b7219905156dc
Daniele Mazzotta <daniele.mazzotta@tii.ae>
2023-06-10 12:37:09 +0400

    added title and description to silenciometer

--- pages/silenciometer.tsx
+++ pages/silenciometer.tsx
@@ -1,6 +1,18 @@
 import { App } from "../projects/silenciometer/App";
+import Head from "next/head";
+import { Lato } from "next/font/google";
 
+const lato = Lato({ subsets: ["latin"], weight: ["100", "400", "700"] });
 
 export default function SilenciometerPage(): JSX.Element {
-  return <App/>;
+  return (
+    <main className={lato.className}>
+      <Head>
+        <title>{"Silenciometer"}</title>
+        <meta name="description" content="Quiet! I'm trying to figure out what noise does a giraffe make"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+      </Head>
+      <App/>
+    </main>
+  )
 }
