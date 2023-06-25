9b8b29702bff656bad053d13b15fd2506f81e56f
Daniele Mazzotta <stichiboi@gmail.com>
2023-06-25 13:01:58 +0400

    fixed title font size and used also in projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,6 +3,7 @@ import styles from "../styles/Projects.module.css";
 import { useTranslation } from "next-export-i18n";
 import { ProjectCard } from "./ProjectCard";
 import { Cube } from "./Cube";
+import { Title } from "./generic/Title";
 
 export default function Projects(): JSX.Element {
   const { t } = useTranslation();
@@ -11,7 +12,8 @@ export default function Projects(): JSX.Element {
   return (
     <section id={"projects"} className={styles.container} ref={scrollRef}>
       <div className={`${styles.content} ${styles.about}`}>
-        <h3 className={styles.title}>{t("projects.about.title")}</h3>
+        <Title text={t("projects.about.title")}/>
+        {/*<h3 className={styles.title}>{t("projects.about.title")}</h3>*/}
         <p>{t("projects.about.text")}</p>
       </div>
       <Cube/>
