88b38511ff1513ee63de48350ac03b072d0cd264
Daniele Mazzotta <stichiboi@gmail.com>
2024-01-19 13:41:04 +0100

    added custom parodle facivon and apple touch icon

--- pages/parodle.tsx
+++ pages/parodle.tsx
@@ -1,8 +1,8 @@
-import { App } from "../projects/parodle/App";
+import {App} from "../projects/parodle/App";
 import Head from "next/head";
-import { Lato } from "next/font/google";
+import {Lato} from "next/font/google";
 
-const lato = Lato({ subsets: ["latin"], weight: ["100", "400", "700"] });
+const lato = Lato({subsets: ["latin"], weight: ["100", "400", "700"]});
 
 export default function ParodlePage() {
   return (
@@ -11,6 +11,8 @@ export default function ParodlePage() {
         <title>{"Parodle"}</title>
         <meta name="description" content="Wordle but with parole (it's in italian)"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/parodle-apple-touch-icon.png"/>
+        <link rel="icon" href={"/parodle-favicon.ico"}/>
       </Head>
       <App/>
     </main>
