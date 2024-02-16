cc7e02eb454d22c4a9da2d45b314f985aa19ca74
Daniele Mazzotta <stichiboi@gmail.com>
2024-02-16 20:56:38 +0100

    added stichisand to the project page

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,61 +1,70 @@
-import React, { useRef } from "react";
+import React, {useRef} from "react";
 import styles from "../styles/Projects.module.css";
-import { useTranslation } from "next-export-i18n";
-import { ProjectCard } from "./ProjectCard";
-import { Stack } from "./Stack";
-import { Title } from "./generic/Title";
+import {useTranslation} from "next-export-i18n";
+import {ProjectCard} from "./ProjectCard";
+import {Stack} from "./Stack";
+import {Title} from "./generic/Title";
 
 export default function Projects(): JSX.Element {
-  const { t } = useTranslation();
-  const scrollRef = useRef<HTMLDivElement | null>(null);
+    const {t} = useTranslation();
+    const scrollRef = useRef<HTMLDivElement | null>(null);
 
-  return (
-    <section id={"projects"} className={styles.container} ref={scrollRef}>
-      <div className={`${styles.content} ${styles.about}`}>
-        <Title text={t("projects.about.title")}/>
-        <p>{t("projects.about.text")}</p>
-      </div>
-      <Stack/>
-      <ProjectCard
-        index={1}
-        target={"/sudoku"}
-        title={t("projects.sudoku.title")}
-        imageSrc={"/sudoku-hero.png"}
-        imageAlt={t("projects.sudoku.alt")}
-      >
-        {t("projects.sudoku.description")}
-      </ProjectCard>
-      <ProjectCard
-        index={2}
-        target={"/parodle"}
-        title={t("projects.parodle.title")}
-        imageSrc={"/parodle-hero.png"}
-        imageAlt={t("projects.parodle.alt")}
-      >
-        {t("projects.parodle.with_sup")}
-        <sup>*</sup>
-        {t("projects.parodle.description")}
-        <br/>
-        <em><sup>*</sup>{t("projects.parodle.disclaimer")}</em>
-      </ProjectCard>
-      <ProjectCard
-        index={3}
-        target={"/silenciometer"}
-        title={t("projects.silenciometer.title")}
-        imageSrc={"/silenciometer-hero.png"}
-        imageAlt={t("projects.silenciometer.alt")}
-      >
-        {t("projects.silenciometer.description")}
-      </ProjectCard>
-      <ProjectCard
-        index={4}
-        target={"/reflexo"}
-        title={t("projects.reflexo.title")}
-        imageSrc={"/reflexo-hero.png"}
-        imageAlt={t("projects.reflexo.alt")}
-      >
-        {t("projects.reflexo.description")}
-      </ProjectCard>
-    </section>
-  );
+    return (
+        <section id={"projects"} className={styles.container} ref={scrollRef}>
+            <div className={`${styles.content} ${styles.about}`}>
+                <Title text={t("projects.about.title")}/>
+                <p>{t("projects.about.text")}</p>
+            </div>
+            <Stack/>
+            <ProjectCard
+                index={1}
+                target={"/stichisand"}
+                title={t("projects.stichisand.title")}
+                imageSrc={"/stichisand-hero.png"}
+                imageAlt={t("projects.stichisand.alt")}
+            >
+                {t("projects.stichisand.description")}
+            </ProjectCard>
+            <ProjectCard
+                index={2}
+                target={"/sudoku"}
+                title={t("projects.sudoku.title")}
+                imageSrc={"/sudoku-hero.png"}
+                imageAlt={t("projects.sudoku.alt")}
+            >
+                {t("projects.sudoku.description")}
+            </ProjectCard>
+            <ProjectCard
+                index={3}
+                target={"/parodle"}
+                title={t("projects.parodle.title")}
+                imageSrc={"/parodle-hero.png"}
+                imageAlt={t("projects.parodle.alt")}
+            >
+                {t("projects.parodle.with_sup")}
+                <sup>*</sup>
+                {t("projects.parodle.description")}
+                <br/>
+                <em><sup>*</sup>{t("projects.parodle.disclaimer")}</em>
+            </ProjectCard>
+            <ProjectCard
+                index={4}
+                target={"/silenciometer"}
+                title={t("projects.silenciometer.title")}
+                imageSrc={"/silenciometer-hero.png"}
+                imageAlt={t("projects.silenciometer.alt")}
+            >
+                {t("projects.silenciometer.description")}
+            </ProjectCard>
+            <ProjectCard
+                index={5}
+                target={"/reflexo"}
+                title={t("projects.reflexo.title")}
+                imageSrc={"/reflexo-hero.png"}
+                imageAlt={t("projects.reflexo.alt")}
+            >
+                {t("projects.reflexo.description")}
+            </ProjectCard>
+        </section>
+    );
 }
