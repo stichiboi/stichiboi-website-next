4121c3fefda2dbb15e2025de273af3964b2b741d
Daniele Mazzotta <stichiboi@gmail.com>
2024-02-16 18:28:10 +0100

    fixed locked loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
-import {useEffect, useRef, useState} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import {useRouter} from "next/router";
 import LoadingScreen from "../components/LoadingScreen";
 import {IconoirProvider} from "iconoir-react";
@@ -8,56 +8,65 @@ import {IconoirProvider} from "iconoir-react";
 const MIN_LOAD_TIME = 1000;
 
 export interface RootAppComponentProps {
-  isLoading: boolean,
-  lockLoading: (v: boolean) => unknown
+    isLoading: boolean,
+    lockLoading: (v: boolean) => unknown
 }
 
 function RootApp({Component, pageProps}: AppProps) {
-  const [isLoading, setIsLoading] = useState(true);
-  const router = useRouter();
-  const isLoadingLockedRef = useRef(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const router = useRouter();
+    const isLoadingLockedRef = useRef(false);
 
-  useEffect(() => {
-    setTimeout(() => setIsLoading(false), MIN_LOAD_TIME);
-  }, []);
+    const stopLoading = useCallback(() => {
+        setIsLoading(isLoadingLockedRef.current);
+        if (isLoadingLockedRef.current) {
+            setTimeout(stopLoading, MIN_LOAD_TIME / 5);
+        }
+    }, []);
 
-  useEffect(() => {
-    let recentSwitch = false;
-    const handleStart = () => {
-      recentSwitch = true;
-      setIsLoading(true);
-      setTimeout(() => recentSwitch = false, MIN_LOAD_TIME);
-    }
-    const handleStop = () => {
-      if (!recentSwitch && !isLoadingLockedRef.current) {
-        setIsLoading(false);
-      } else {
-        // check at intervals
-        setTimeout(handleStop, MIN_LOAD_TIME / 5);
-      }
-    }
+    useEffect(() => {
+        setTimeout(stopLoading, MIN_LOAD_TIME);
+    }, [stopLoading]);
 
-    router.events.on('routeChangeStart', handleStart)
-    router.events.on('routeChangeComplete', handleStop)
-    router.events.on('routeChangeError', handleStop)
+    useEffect(() => {
+        let recentSwitch = false;
+        const handleStart = () => {
+            recentSwitch = true;
+            setIsLoading(true);
+            setTimeout(() => recentSwitch = false, MIN_LOAD_TIME);
+        }
+        const handleStop = () => {
+            if (!recentSwitch && !isLoadingLockedRef.current) {
+                setIsLoading(false);
+            } else {
+                // check at intervals
+                setTimeout(handleStop, MIN_LOAD_TIME / 5);
+            }
+        }
 
-    return () => {
-      router.events.off('routeChangeStart', handleStart)
-      router.events.off('routeChangeComplete', handleStop)
-      router.events.off('routeChangeError', handleStop)
-    }
-  }, [router, isLoadingLockedRef]);
+        router.events.on('routeChangeStart', handleStart)
+        router.events.on('routeChangeComplete', handleStop)
+        router.events.on('routeChangeError', handleStop)
 
-  return (
-    <IconoirProvider
-      iconProps={{
-        // iOS sets the color of svg to blue by default
-        color: "currentColor"
-      }}>
-      <LoadingScreen isLoading={isLoading}/>
-      <Component {...pageProps} isLoading={isLoading} lockLoading={(v: boolean) => isLoadingLockedRef.current = v}/>
-    </IconoirProvider>
-  )
+        return () => {
+            router.events.off('routeChangeStart', handleStart)
+            router.events.off('routeChangeComplete', handleStop)
+            router.events.off('routeChangeError', handleStop)
+        }
+    }, [router, isLoadingLockedRef, setIsLoading]);
+
+    return (
+        <IconoirProvider
+            iconProps={{
+                // iOS sets the color of svg to blue by default
+                color: "currentColor"
+            }}>
+            <LoadingScreen isLoading={isLoading}/>
+            <Component {...pageProps} isLoading={isLoading} lockLoading={(v: boolean) => {
+                isLoadingLockedRef.current = v;
+            }}/>
+        </IconoirProvider>
+    )
 }
 
 export default RootApp
