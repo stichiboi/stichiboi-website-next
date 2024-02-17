6d539562285c5e3d9c54191a7ec674451793d8cc
Daniele Mazzotta <daniele.mazzotta@tii.ae>
2024-02-17 10:05:25 +0100

    try refactoring

--- pages/api/stichisand/grid.ts
+++ pages/api/stichisand/grid.ts
@@ -1,59 +1,70 @@
-import {NextApiRequest, NextApiResponse} from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import clientPromise from "../../../lib/mongodb";
-import {ObjectId} from "mongodb"
+import { ObjectId } from "mongodb"
 
 
 export type GridData = {
-    version: string,
-    grid: string,
-    width: number,
-    height: number,
-    name: string,
-    creatorName: string,
-    id?: string
+  version: string,
+  grid: string,
+  width: number,
+  height: number,
+  name: string,
+  creatorName: string,
+  id?: string
 }
 
 const DB_NAME = "stichisand";
 const COLLECTION_NAME = "grid";
 
-// GET
-export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<GridData | { id: string }>
-) {
-    try {
-        const {method} = req;
-        const client = await clientPromise;
-        const db = client.db(DB_NAME);
-        const collection = db.collection(COLLECTION_NAME)
-
-        if (method === "GET") {
-            const {id} = req.query;
-            if (!id) {
-                return res.status(200);
-            }
-            const _id = new ObjectId(id as string);
-            const grid = await collection.findOne({_id});
-            if (grid) {
-                return res.status(200).json(grid as unknown as GridData);
-            }
-            return res.status(404);
-        } else if (method === "POST") {
-            const body = JSON.parse(req.body) as GridData;
-            let {id, ...rawBody} = body;
-            if (id) {
-                const _id = new ObjectId(id);
-                await collection.updateOne({_id}, {"$set": {...rawBody, updated: new Date()}});
-            } else {
-                const doc = await collection.insertOne({...rawBody, created: new Date()});
-                id = doc.insertedId.toString();
-            }
-            return res.status(200).json({id});
-        }
-        return res.status(405);
-    } catch (e) {
-        console.error(e);
-        res.status(400);
-    }
+function getCollection() {
+  return clientPromise
+    .then(client => client.db(DB_NAME))
+    .then(db => db.collection(COLLECTION_NAME));
 }
 
+export function getGrid(id: string | string[] | undefined | null) {
+  if (typeof id !== "string") {
+    return Promise.resolve(undefined);
+  }
+  return getCollection()
+    .then(collection => {
+      const _id = new ObjectId(id as string);
+      return collection.findOne({ _id });
+    }).then(doc => {
+      return doc as unknown as (GridData | undefined);
+    });
+}
+
+export function GET(
+  req: NextApiRequest,
+  res: NextApiResponse<GridData>
+) {
+  const { id } = req.query;
+  return getGrid(id)?.then(grid => {
+    if (grid) {
+      return res.status(200).json(grid as unknown as GridData);
+    }
+    return res.status(404);
+  });
+}
+
+export function POST(
+  req: NextApiRequest,
+  res: NextApiResponse<{ id: string }>
+) {
+  return getCollection()
+    .then(collection => {
+      const body = JSON.parse(req.body) as GridData;
+      const { id, ...rawBody } = body;
+      if (id) {
+        const _id = new ObjectId(id);
+        return collection.updateOne({ _id }, { "$set": { ...rawBody, updated: new Date() } })
+          .then(() => id);
+      }
+      return collection.insertOne({ ...rawBody, created: new Date() })
+        .then(doc => doc.insertedId.toString());
+    }).then(id => {
+      return res.status(200).json({ id });
+    });
+
+}
